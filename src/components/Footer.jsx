41e3216ffded54cd63d4e028bc14f64abb28ee87
unknown <daniellopezj0327@gmail.com>
2021-01-08 07:41:36 -0500

    ...

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => (
+  <div className="Footer">
+    <p className="Footer-title">nombre aplicacion</p>
+    <p className="Foote-copy">todos los derechos reservador</p>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/Header.css';
+
+const Header = () => (
+  <div className="Header">
+    <h1>esto es un header </h1>
+    <div className="header-checkout">Checkout</div>
+  </div>
+);
+
+export default Header;
